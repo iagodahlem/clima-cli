22ad4a4a367687dac81c2a1ef22187a4bba41eca
Iago Dahlem Lorensini <iagodahlemlorensini@gmail.com>
2018-05-22 16:50:54 -0300

    Add `forecast` command to main help menu

--- cmds/help.js
+++ cmds/help.js
@@ -3,19 +3,19 @@ const menus = {
     tiempo [command] <options>
 
     today .............. show weather for today
+    forecast ........... show 10-day weather forecast
     version ............ show package version
-    help ............... show help menu for a command
-  `,
+    help ............... show help menu for a command`,
+
   today: `
     tiempo today <options>
 
-    --location, -l ..... the location to use
-  `,
+    --location, -l ..... the location to use`,
+
   forecast: `
     tiempo forecast <options>
 
-    --location, -l ..... the location to use
-  `,
+    --location, -l ..... the location to use`,
 }
 
 module.exports = (args) => {
